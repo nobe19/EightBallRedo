43935f3be66a0d1d623258a90788f5699947a3d9
Nipuni Obe <obe@gmail.com>
2021-03-01 17:33:56 -0500

    repeat while

--- EightBall/ViewController.swift
+++ EightBall/ViewController.swift
@@ -42,13 +42,13 @@ class ViewController: UIViewController {
     func getResult() {
         resultLabel.alpha = 0.0
         
-        var randomIndex: Int //var randomIndex = -1 is okay too
-        repeat {
+        var randomIndex = Int.random(in: 0..<answerArray.count)
+        while randomIndex == lastIndex {
             randomIndex = Int.random(in: 0..<answerArray.count)
             if randomIndex == lastIndex {
                 print("We just repeated \(answerArray[lastIndex])")
             }
-        } while randomIndex == lastIndex
+        }
         lastIndex = randomIndex
         resultLabel.text = answerArray[lastIndex]
         UIView.animate(withDuration: 1.0, animations: {self.resultLabel.alpha = 1.0})
